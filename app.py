46436cfb0e1516c561aac146f65cc9e5c73d73e4
DMT My Hiep <dmt.myhiep@gmail.com>
2026-09-07 11:08:31 +0700

    Fix NameError px by importing plotly.express at top of app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
 from datetime import datetime, timedelta, time
 import os
 import io
